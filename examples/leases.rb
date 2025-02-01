4c39d3513406c3f88df408c08fbcb3e547d745f1
Wes Hays <weshays@gmail.com>
2025-01-31 16:55:33 -0800

    Added support for specifying the tenant ID to get the information for

--- examples/leases.rb
+++ examples/leases.rb
@@ -15,9 +15,9 @@ rv_client = Rentvine::Client.new(auth)
 # Lease Examples
 # =========================================
 
-rv_client.leases.each do |lease|
-  puts lease
-end
+# rv_client.leases.each do |lease|
+#   puts lease
+# end
 
 # ===========================
 
@@ -36,3 +36,17 @@ rv_client.export_leases.each do |lease|
   binding.pry
   adsf=3
 end
+
+# ===========================
+
+lease_id = 1
+rv_client.export_leases(lease_ids: [lease_id]).each do |lease|
+  binding.pry
+  adsf=3
+end
+
+# ===========================
+
+# Filter by lease_ids
+lease_ids = [1,2]
+leases = rv_client.export_leases(leaseIDs: lease_ids)
